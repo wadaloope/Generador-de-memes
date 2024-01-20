8b916ed0cb17ca0ad833e489b863fa62ef086326
wadaloope <139147384+wadaloope@users.noreply.github.com>
2024-01-20 01:51:38 -0300

    Se incorporó la funcionalidad de carga de la imagen fuente desde disco rigido. También se corrigieron los estilos del div que la aloja, para optimizar su presentación

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const btnTxt = document.getElementsByClassName('boton-menu')[1];
 const btnModo = document.getElementsByClassName('boton-menu')[2];
 const panelImg = document.getElementsByClassName('panel-editor')[0];
 const panelTxt = document.getElementsByClassName('panel-editor')[1];
+const fileInput = document.getElementById("fileInput");
+const downloadBtn = document.getElementById("downloadBtn");
 
 //↓↓↓-Seleccion y ocultamiento del panel para edicion de imagen-↓↓↓
 
 let edicImg = false;
 let edicTxt = false;
-btnImg.addEventListener("click", (e) => { 
+btnImg.addEventListener("click", (e) => {
     if (!edicImg) {
         panelImg.classList.add('panelOn');
         panelTxt.classList.remove('panelOn');
@@ -42,9 +44,38 @@ btnTxt.addEventListener("click", (e) => {
     }
 })
 
-//↓↓↓-Activacion y descativacion del modo noctorno-↓↓↓
+//↓↓↓-Activacion y descativacion del modo nocturno-↓↓↓
+let nocheEstado = true;
 
 btnModo.addEventListener("click", (e) => {
-    document.getElementsByTagName('body')[0].classList.toggle("dia");
+    if (nocheEstado) {
+        document.getElementsByTagName('body')[0].classList.add("dia");
+        document.getElementsByClassName('modo_o')[0].innerHTML = "claro";
+        document.getElementById("imgTag").style.filter = "invert(100%)";
+        nocheEstado = false;
+    }
+    else {
+        document.getElementsByTagName('body')[0].classList.remove("dia");
+        document.getElementsByClassName('modo_o')[0].innerHTML = "oscuro";
+        document.getElementById("imgTag").style.filter = "none";
+        nocheEstado = true;
+    }
+})
+
+//↓↓↓-Cargado de imagen desde disco duro-↓↓↓
+
+fileInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    console.dir(e.target);
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            const imgElement = document.getElementById("imgTag");
+            imgElement.src = event.target.result;
+            document.getElementById('label').style.color = "#00000000";
+        };
+        reader.readAsDataURL(file);
+    }
+
 })
 
